e1c43148169652a78902218c7a1142fe0a357025
Robert Brown <robert.brown@gmail.com>
2012-02-03 19:11:12 -0500

    helpers.cc: Delete unneeded #include. wire-format.lisp: Fix copyright date.

--- wire-format.lisp
+++ wire-format.lisp
@@ -1,4 +1,4 @@
-;;;; Copyright 2011 Google Inc.  All Rights Reserved
+;;;; Copyright 2010 Google Inc.  All Rights Reserved
 
 ;;;; Redistribution and use in source and binary forms, with or without
 ;;;; modification, are permitted provided that the following conditions are
