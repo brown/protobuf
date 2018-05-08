c74c9c955bc55e13dc857329e75bc82fa7024583
cc <longlene@gmail.com>
2018-05-08 17:00:41 +0800

    fix for protobuf 3.5.2

--- protoc/lisp/primitive_field.cc
+++ protoc/lisp/primitive_field.cc
@@ -32,6 +32,7 @@
 #include "helpers.h"
 #include <google/protobuf/io/printer.h>
 #include <google/protobuf/wire_format.h>
+#include <google/protobuf/descriptor.pb.h>
 #include "strutil.h"
 
 namespace google {
